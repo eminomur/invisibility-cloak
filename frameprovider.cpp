6762410c964821538eb5cdcdb8eeb3226a7f9c19
Muhammed Emin ÖMÜR <m.eminomur@outlook.com>
2020-05-06 06:21:39 +0300

    Add frame provider class definition

--- frameprovider.cpp
+++ frameprovider.cpp
@@ -1,6 +1,30 @@
 #include "frameprovider.h"
 
-FrameProvider::FrameProvider(QWidget *parent) : QWidget(parent)
-{
+#include <opencv2/imgproc.hpp>
 
+FrameProvider::FrameProvider(int index, QWidget *parent)
+    : QWidget(parent), video_capture(index), shooting_flag(true)
+{
+    if (!video_capture.isOpened()) {
+        throw CameraNotFoundException();
+    }
+
+    frame_provider_thread = QThread::create([&]() {
+        cv::Mat frame;
+        while (shooting_flag) {
+            video_capture >> frame;
+            cv::resize(frame, frame, cv::Size(600, 600), 0, 0, cv::INTER_AREA);
+            cv::flip(frame, frame, 1);
+            emit new_frame_ready(frame);
+        }
+    });
+
+    connect(frame_provider_thread, &QThread::finished, frame_provider_thread, &QThread::deleteLater);
+    frame_provider_thread->start();
+}
+
+FrameProvider::~FrameProvider()
+{
+    shooting_flag = false;
+    frame_provider_thread->wait();
 }
--- frameprovider.h
+++ frameprovider.h
@@ -2,15 +2,38 @@
 #define FRAMEPROVIDER_H
 
 #include <QWidget>
+#include <QThread>
+
+#include <exception>
+
+#include <opencv2/videoio.hpp>
+#include <opencv2/core.hpp>
+
+class CameraNotFoundException : public std::exception
+{
+public:
+    virtual const char *what() const noexcept override
+    {
+        return "No camera found for given index";
+    }
+};
 
 class FrameProvider : public QWidget
 {
     Q_OBJECT
 public:
-    explicit FrameProvider(QWidget *parent = nullptr);
+    explicit FrameProvider(int index = 0, QWidget *parent = nullptr);
+    ~FrameProvider();
 
 signals:
+    void new_frame_ready(const cv::Mat&);
 
+private:
+    QThread *frame_provider_thread;
+
+    cv::VideoCapture video_capture;
+
+    bool shooting_flag;
 };
 
 #endif // FRAMEPROVIDER_H
